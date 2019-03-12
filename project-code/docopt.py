92ac0836a0511a93ea673fd69d6f123e150f65e4
Gregor von Laszewski <laszewski@gmail.com>
2019-03-12 17:55:47 -0400

    Update docopt.py

--- project-code/docopt.py
+++ project-code/docopt.py
@@ -9,9 +9,9 @@
                 storage [--storage=SERVICE] info FILENAME
                 storage [--storage=SERVICE] create FILENAME
                 storage [--storage=SERVICE] search FILENAME
-                storage [--storage=SERVICE] createdir DIRNAME DESTDIR
-                storage [--storage=SERVICE] listdir DIRNAME
-                storage [--storage=SERVICE] deletedir DIRNAME
+                storage [--storage=SERVICE] create dir DIRNAME DESTDIR
+                storage [--storage=SERVICE] list dir DIRNAME
+                storage [--storage=SERVICE] delete dir DIRNAME
 
 
           This command creates, deletes, and returns information on directories and files in cloud storage services.
